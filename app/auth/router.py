44a39da98e814cc611288c9cfbde95a18b8451f7
디마 <dmitriiafanasev@proton.me>
2026-07-31 11:18:08 +0200

    CS-2

--- app/auth/router.py
+++ app/auth/router.py
@@ -1,22 +1,27 @@
 import secrets
 
-from fastapi import APIRouter, HTTPException, status
+from fastapi import Depends, APIRouter, HTTPException, status
+from fastapi.security import OAuth2AuthorizationCodeBearer
 
 from ..db.mongodb import users
 from .schemas import AuthUser
-from ..users.schemas import Users
 
 auth_router = APIRouter()
 
 @auth_router.get(
         "/get_user/{username}", 
-        # response_model=Users,
         response_model_by_alias=False,
         tags=["Demo auth"])
 def get_auth_user_username(username : str) -> str:
-    corrent_user_db = users.find_one({"username" : username})
-    corrent_user = Users.model_validate(corrent_user_db)
+    corrent_user_db = users.find_one({"username" : username}, {"id" : 1, "email" : 1 , "username": 1, "hashed_password" : 1})
+    corrent_user = AuthUser.model_validate(corrent_user_db)
 
     ###### U should create  check for password with corrent user
+
+    """
+    1. Попробовать взять модель pydantic но только сам хешированый пароль
+        создание новой модели
+    2. 
+    """
 
     return corrent_user.hashed_password
--- app/auth/schemas.py
+++ app/auth/schemas.py
@@ -1,6 +1,11 @@
-from pydantic import BaseModel, EmailStr
+from typing import Annotated, Optional
+
+from pydantic import BaseModel, EmailStr, BeforeValidator, Field
+
+PyObjectId = Annotated[str, BeforeValidator(str)]
 
 class AuthUser(BaseModel):
+    id : Optional[PyObjectId] = Field(alias="_id", default=None)
     email : EmailStr
-    user_name : str 
+    username : str 
     hashed_password : str
